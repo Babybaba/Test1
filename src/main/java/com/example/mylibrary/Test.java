2800c2470a17306caa5177691df57132dd3376fe
Babybab <316067260@qq.com>
2019-03-15 13:28:50 +0800

    测试提交

---
+++ src/main/java/com/example/mylibrary/Test.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+    public static void Toast(Context context){
+        Toast.makeText(context, "测试！！！", Toast.LENGTH_SHORT).show();
+    }
+}
